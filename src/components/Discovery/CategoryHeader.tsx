f6e56e9d30ce7bcf079e121987b529c965344ab9
royacodes <roya.raastgar@gmail.com>
2024-09-25 15:16:20 +0330

    design discovery page

--- src/components/Discovery/CategoryHeader.tsx
+++ src/components/Discovery/CategoryHeader.tsx
@@ -6,7 +6,7 @@ import { IoIosArrowForward } from "react-icons/io";
 const CategoryHeader: FC<CategoryHeaderProps> = ({ title, handleMore }) => {
   return (
    <>
-   <button className='w-full flex justify-between bg-white shadow-slate-300 shadow-sm items-center rounded-3xl mb-4 text-gray-700 p-2'>
+   <button className='w-full flex justify-between bg-white shadow-slate-300 shadow-sm items-center rounded-3xl mb-4 mt-4 text-gray-700 p-2'>
     {title}
    <IoIosArrowForward />
    </button>

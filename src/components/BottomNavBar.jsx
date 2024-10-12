e1b49900155cecc73388e9527ea22fbd59b5a536
royacodes <roya.raastgar@gmail.com>
2024-10-12 13:57:02 +0330

    fix bottom navigation items color

--- src/components/BottomNavBar.jsx
+++ src/components/BottomNavBar.jsx
@@ -1,15 +1,7 @@
 import { NavLink } from "react-router-dom";
-import { IoPersonAddSharp } from "react-icons/io5";
-import { MdDashboard } from "react-icons/md";
-import { PiHandWithdrawFill } from "react-icons/pi";
-import { FaCoins } from "react-icons/fa";
-import { ImProfile } from "react-icons/im";
-import globeIcon from "../assets/globe.svg";
 import { FaGlobeAmericas } from "react-icons/fa";
 import { RiAiGenerate } from "react-icons/ri";
-import profile from "../assets/profile.webp";
 import { LuWallet } from "react-icons/lu";
-import { IoGameController } from "react-icons/io5";
 import { SiSololearn } from "react-icons/si";
 import { LuBot } from "react-icons/lu";
 
@@ -20,9 +12,13 @@ const BottomNavBar = () => {
         <button className="flex flex-col items-center bg-transparent">
           <NavLink
             to="/earn"
-            className="flex flex-col items-center bg-transparent"
+            className={({ isActive }) =>
+              `flex flex-col items-center ${
+                isActive ? "text-white" : "text-gray-400"
+              }`
+            }
           >
-            <SiSololearn className="text-2xl text-white bg-transparent" />
+            <SiSololearn className="text-2xl" />
             {/* <p className="text-gray-400 text-xs mt-[1px]">Earn</p> */}
           </NavLink>
         </button>
@@ -30,9 +26,13 @@ const BottomNavBar = () => {
         <button className="flex flex-col items-center bg-transparent">
           <NavLink
             to="/cards"
-            className="flex flex-col items-center bg-transparent"
+            className={({ isActive }) =>
+              `flex flex-col items-center ${
+                isActive ? "text-white" : "text-gray-400"
+              }`
+            }
           >
-            <RiAiGenerate className="text-3xl text-white bg-transparent" />
+            <RiAiGenerate className="text-3xl " />
             {/* <p className="text-gray-400 text-xs mt-[1px]">Cards</p> */}
           </NavLink>
         </button>
@@ -46,10 +46,7 @@ const BottomNavBar = () => {
         {/* Circular Button in the center */}
         <div className="absolute -top-8 left-1/2 transform -translate-x-1/2">
           <button className="w-16 h-16 bg-primary-color text-white rounded-full shadow-lg flex items-center justify-center border-4 border-white">
-            <NavLink
-              to="/discovery_app"
-              className="flex items-center justify-center"
-            >
+            <NavLink to="/" className="flex items-center justify-center">
               <FaGlobeAmericas className="text-5xl" />
             </NavLink>
           </button>
@@ -62,8 +59,12 @@ const BottomNavBar = () => {
 
         <button className="flex flex-col items-center bg-transparent">
           <NavLink
-            to="/discovery_app"
-            className="flex flex-col items-center justify-center"
+            to="/bot"
+            className={({ isActive }) =>
+              `flex flex-col items-center ${
+                isActive ? "text-white" : "text-gray-400"
+              }`
+            }
           >
             <LuBot className="text-3xl" />
             {/* <p className="text-gray-400 text-xs mt-[1px]">Game</p> */}
@@ -71,8 +72,12 @@ const BottomNavBar = () => {
         </button>
         <button className="flex flex-col items-center bg-transparent">
           <NavLink
-            to="/discovery_app"
-            className="flex flex-col items-center justify-center"
+            to="/wallet"
+            className={({ isActive }) =>
+              `flex flex-col items-center ${
+                isActive ? "text-white" : "text-gray-400"
+              }`
+            }
           >
             <LuWallet className="text-3xl" />
             {/* <p className="text-gray-400 text-xs mt-[1px]">Wallet</p> */}
